6a2f97d205a57a12af1b2595276c08ee88e2263a
Gultaj Muradova 14125 <gultajmuradova14125@ada.edu.az>
2024-02-20 13:35:52 +0400

    create spring-app

---
+++ first-spring-app/src/main/java/az/edu/ada/wm2/firstspringapp/FirstSpringSppApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.firstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringSppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringSppApplication.class, args);
+	}
+
+}
