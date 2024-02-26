e2fd470bb7f3fcb1f18fa3af10a6501b7a300ac8
Gultaj Muradova 14125 <gultajmuradova14125@ada.edu.az>
2024-02-26 22:43:57 +0400

    create CRUD app for library system with Spring boot (maven) and thymeleaf

---
+++ crud-library-app/src/main/java/az/edu/ada/mw2/crudlibraryapp/crudlibraryapp/CrudLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.mw2.crudlibraryapp.crudlibraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudLibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudLibraryAppApplication.class, args);
+	}
+
+}
---
+++ crud-library-app/src/test/java/az/edu/ada/mw2/crudlibraryapp/crudlibraryapp/CrudLibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.mw2.crudlibraryapp.crudlibraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudLibraryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
